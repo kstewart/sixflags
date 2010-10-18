341c7f0930cefe66b2266ef85486997d41f4905d
Kevin Stewart <kevin@working-code.com>
2010-10-17 17:03:07 -0700

    Refactored middleware to use Redis instead of YAML.

--- lib/sixflags.rb
+++ lib/sixflags.rb
@@ -1,25 +1,29 @@
 require 'rack'
-require 'yaml'
+require 'redis'
 
 class SixFlags
   def initialize(app, options)
     @app      = app
     @options  = options
-    if @options[:config]
-      @config = YAML.load(File.open(@options[:config]))
-    else
-      @options[:config] = nil
-    end
+    host = @options[:host] ? @options[:host]:"127.0.0.1"
+    port = @options[:port] ? @options[:port]:6789
+    
+    @config = Redis.new(:host => host, :port => port)
   end
+  
+  # TODO: Look at using Redis' hash features to store config
+  # TODO: Add a prefix to the keys
   
   def call(env)
     req   = Rack::Request.new(env)
     scope = req.env["RACK_ENV"] ? req.env["RACK_ENV"]:"test"
     url   = req.path
+    # request_method = env['REQUEST_METHOD']
+    # if ['GET', 'POST', 'DELETE', 'PUT', 'HEAD'].include?(request_method)
     
-    if @config[scope][url] == true
+    if @config[url] == "true"
       @app.call(env)
-    elsif @config[scope][url] == false
+    elsif @config[url] == "false"
       [403, {}, ["Feature disabled."]]
     else
       [404, {}, ["Feature not found."]]
--- test/test_sixflags.rb
+++ test/test_sixflags.rb
@@ -1,10 +1,17 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+
 require 'rubygems'
-require 'test/unit'
+require 'minitest/autorun'
 require 'rack'
 require 'rack/test'
+require 'redis'
 require 'sixflags'
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+class MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+end
+
+ENV['RACK_ENV'] = 'test'
 
 class TestApp  
   def call(env)  
@@ -12,41 +19,59 @@ class TestApp
   end  
 end
 
-class SixFlagsTest < Test::Unit::TestCase
+class SixFlagsTest < MiniTest::Unit::TestCase
   include Rack::Test::Methods
   
   def app
     @test_app = TestApp.new
     
-    SixFlags.new(@test_app, {:config => File.join(File.dirname(__FILE__), 'config.yml')})
+    SixFlags.new(@test_app, {:host => "127.0.0.1", :port => 6379})
   end
 
+  def setup
+    config = Redis.new
+    config.set "/", true       
+    config.set "/hello", true
+    config.set "/goodbye", false
+    config.set "/enabled", true
+    config.set "/disabled", false
+  end
+  
+  def teardown
+    config = Redis.new
+    config.del "/"        
+    config.del "/hello"
+    config.del "/goodbye"
+    config.del "/enabled"
+    config.del "/disabled"
+  end
+  
   def test_enabled
     get '/'
-    assert last_response.ok?
+    assert last_response.ok?, "Expected 200, got #{last_response.status}"
     assert_equal 'Hello world!', last_response.body
 
     get '/enabled'
-    assert last_response.ok?
+    assert last_response.ok?, "Expected 200, got #{last_response.status}"
     assert_equal 'Hello world!', last_response.body
     
-    post '/enabled'
+    post '/enabled', "Expected 200, got #{last_response.status}"
     assert last_response.ok?
   end
   
   def test_disabled
     get '/goodbye'
-    assert last_response.forbidden?
+    assert last_response.forbidden?, "Expected 403, got #{last_response.status}"
     assert_equal 'Feature disabled.', last_response.body
     
     delete '/goodbye'
-    assert last_response.forbidden?
+    assert last_response.forbidden?, "Expected 403, got #{last_response.status}"
     assert_equal 'Feature disabled.', last_response.body
   end
 
   def test_is_not_found
     get '/foo'
-    assert last_response.not_found?
+    assert last_response.not_found?, "Expected 404, got #{last_response.status}"
     assert_equal 'Feature not found.', last_response.body
   end
 end
